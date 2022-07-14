5e2cab8eb921d4b6cb95c0ed4fbff09c18b3d289
Adrian <adriann.kowalski@gmail.com>
2022-07-14 17:56:50 +0200

    Added main components mockups: NavMenu, Slider, LogoBar, AdsBoxes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import NavMenu from './Components/NavMenu/NavMenu';
+import Slider from './Components/Slider/Slider';
+import LogoBar from './Components/LogoBar/LogoBar';
+import AdsBoxes from './Components/AdsBoxes/AdsBoxes';
 
 function App() {
   return (
     <div className="App">
+      <NavMenu />
+      <Slider />
+      <LogoBar />
+      <AdsBoxes />
     </div>
   );
 }
---
+++ src/Components/AdsBoxes/AdsBoxes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdsBoxes = () => {
+  return (
+    <div>AdsBoxes</div>
+  )
+}
+
+export default AdsBoxes

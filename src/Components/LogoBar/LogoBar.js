e9e80f76c8c2673576bfe77d05aabe2d47f07138
Adrian <adriann.kowalski@gmail.com>
2022-07-17 12:08:57 +0200

    added logos img in LogoBar component

--- src/Components/LogoBar/LogoBar.js
+++ src/Components/LogoBar/LogoBar.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './LogoBar.css';
+import pyszne from '../assets/pyszne.png';
+import deligoo from '../assets/deligoo.png';
+import glovo from '../assets/glovo.png';
+import ubereats from '../assets/ubereats.png';
+import boltfood from '../assets/boltfood.png';
+
+
+
 
 const LogoBar = () => {
   return (
-    <div>LogoBar</div>
+    <div className='LogoBarMainContainer'>
+      <img className='DeliveryLogo' src={pyszne} alt='pyszne.pl'/>
+      <img className='DeliveryLogo' src={deligoo} alt='deligoo.pl'/>
+      <img className='DeliveryLogo' src={glovo} alt='glovo.pl'/>
+      <img className='DeliveryLogo' src={ubereats} alt='ubereats.pl'/>
+      <img className='DeliveryLogo' src={boltfood} alt='boltfood.pl'/>
+    </div>
   )
 }
 

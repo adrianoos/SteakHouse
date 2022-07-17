047554d7b8144bd57fde9fee05ad5510736556bd
Adrian Kowalski <adriann.kowalski@gmail.com>
2022-07-17 18:14:30 +0200

    Delete src directory

--- src/Components/assets/menuPositions.js
+++
@@ -1,3 +0,0 @@
-const MenuPositions = ['Steaks', 'Burgers', 'French Fries', 'Drinks' , 'Gdzie Dowozimy?', 'Kontakt']
-
-export default MenuPositions;
--- src/Components/assets/userButtons.js
+++
@@ -1,7 +0,0 @@
-
-
-const userButtons = [{name: 'Moje Konto', icon: 'person'},
-                     {name: 'Koszyk', icon: 'cart'},
-                     {name: 'Zamów', icon: 'menu'}]
-
-export default userButtons;
